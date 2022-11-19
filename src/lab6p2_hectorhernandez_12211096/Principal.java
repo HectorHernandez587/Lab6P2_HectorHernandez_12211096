7daf023a43032243b934f794ae091e2380b04f51
HectorHernandez587 <eduhp587@gmail.com>
2022-11-18 20:15:34 -0600

    Avance 9

--- src/lab6p2_hectorhernandez_12211096/Principal.java
+++ src/lab6p2_hectorhernandez_12211096/Principal.java
@@ -56,10 +56,11 @@ public class Principal extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         jScrollPane7 = new javax.swing.JScrollPane();
         jl_jugadores = new javax.swing.JList<>();
+        jLabel19 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        bt_comprar_alimento = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jScrollPane8 = new javax.swing.JScrollPane();
         jl_alimentos = new javax.swing.JList<>();
@@ -118,9 +119,19 @@ public class Principal extends javax.swing.JFrame {
         popup_menu.add(Eliminar);
 
         Seleccionar.setText("Seleccionar");
+        Seleccionar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                SeleccionarActionPerformed(evt);
+            }
+        });
         popup_menu.add(Seleccionar);
 
         Enviar_Solicitud.setText("Enviar Solicitud");
+        Enviar_Solicitud.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Enviar_SolicitudActionPerformed(evt);
+            }
+        });
         popup_menu.add(Enviar_Solicitud);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -154,13 +165,19 @@ public class Principal extends javax.swing.JFrame {
         jl_jugadores.setModel(new DefaultListModel());
         jScrollPane7.setViewportView(jl_jugadores);
 
+        jLabel19.setText("Pancho Colindrez");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 157, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel19, javax.swing.GroupLayout.PREFERRED_SIZE, 117, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 40, Short.MAX_VALUE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -179,10 +196,12 @@ public class Principal extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel19))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(1, 1, 1)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -193,7 +212,7 @@ public class Principal extends javax.swing.JFrame {
                         .addGap(38, 38, 38)
                         .addComponent(jScrollPane7, javax.swing.GroupLayout.PREFERRED_SIZE, 194, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 354, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(54, Short.MAX_VALUE))
+                .addContainerGap(28, Short.MAX_VALUE))
         );
 
         jTabbedPane1.addTab("Juego", jPanel1);
@@ -204,11 +223,21 @@ public class Principal extends javax.swing.JFrame {
         jLabel6.setFont(new java.awt.Font("Times New Roman", 1, 12)); // NOI18N
         jLabel6.setText("0");
 
-        jButton1.setFont(new java.awt.Font("Times New Roman", 1, 12)); // NOI18N
-        jButton1.setText("Comprar");
+        bt_comprar_alimento.setFont(new java.awt.Font("Times New Roman", 1, 12)); // NOI18N
+        bt_comprar_alimento.setText("Comprar");
+        bt_comprar_alimento.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                bt_comprar_alimentoMouseClicked(evt);
+            }
+        });
 
         jButton2.setFont(new java.awt.Font("Times New Roman", 1, 12)); // NOI18N
         jButton2.setText("Comprar");
+        jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton2MouseClicked(evt);
+            }
+        });
 
         jl_alimentos.setModel(new DefaultListModel());
         jScrollPane8.setViewportView(jl_alimentos);
@@ -230,7 +259,7 @@ public class Principal extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jLabel6)
                         .addGap(0, 0, Short.MAX_VALUE))
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(bt_comprar_alimento, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
@@ -242,7 +271,7 @@ public class Principal extends javax.swing.JFrame {
                     .addComponent(jLabel5)
                     .addComponent(jLabel6))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton1)
+                .addComponent(bt_comprar_alimento)
                 .addGap(8, 8, 8)
                 .addComponent(jScrollPane8, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
@@ -652,6 +681,36 @@ public class Principal extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_EliminarActionPerformed
 
+    private void SeleccionarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SeleccionarActionPerformed
+        // TODO add your handling code here:
+        if (nodo_seleccionado.getUserObject() instanceof Jugador) {
+            JOptionPane.showMessageDialog(jt_categorias, "Se selecciono a ");
+        }
+    }//GEN-LAST:event_SeleccionarActionPerformed
+
+    private void Enviar_SolicitudActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Enviar_SolicitudActionPerformed
+        // TODO add your handling code here:
+        
+        
+    }//GEN-LAST:event_Enviar_SolicitudActionPerformed
+
+    private void bt_comprar_alimentoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt_comprar_alimentoMouseClicked
+        // TODO add your handling code here:
+        int pos = jl_alimentos.getSelectedIndex();
+        int pos2 = jl_jugadores.getSelectedIndex();
+
+        if (jug.get(pos2).getConsumo() > ali.get(pos).getCosto()) {
+            jug.get(pos2).setAlimento(ali.get(pos));
+            JOptionPane.showMessageDialog(rootPane, "Comprado con exito");
+        } else {
+            JOptionPane.showMessageDialog(rootPane, "No tiene suficiente consumo para comprar eso");
+        }
+    }//GEN-LAST:event_bt_comprar_alimentoMouseClicked
+
+    private void jButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2MouseClicked
+
     /**
      * @param args the command line arguments
      */
@@ -753,12 +812,12 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JMenuItem Seleccionar;
     private javax.swing.JButton bt_aceptar_solicitud;
     private java.awt.Button bt_alimentar;
+    private javax.swing.JButton bt_comprar_alimento;
     private javax.swing.JButton bt_crear_alimento;
     private javax.swing.JButton bt_crear_billetera;
     private javax.swing.JButton bt_crear_categoria;
     private javax.swing.JButton bt_rechazar_solicitud;
     private javax.swing.JComboBox<String> cb_categoria;
-    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JComboBox<String> jComboBox2;
     private javax.swing.JLabel jLabel1;
@@ -771,6 +830,7 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel16;
     private javax.swing.JLabel jLabel17;
     private javax.swing.JLabel jLabel18;
+    private javax.swing.JLabel jLabel19;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
@@ -805,6 +865,7 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JTextField jtf_nomcategoria;
     private javax.swing.JPopupMenu popup_menu;
     // End of variables declaration//GEN-END:variables
+    
     ArrayList <Billetera> bi = new ArrayList();
     ArrayList <Alimento> ali = new ArrayList();
     ArrayList <Categoria> cat = new ArrayList();
